50b1059bcbef8ddef2cff846d68e3477710e5488
Dmg5th <dmgould310@gmail.com>
2021-01-18 10:40:40 -0500

    commit tree 9be63fc1ce26d1aa37e4e289d7b7e2e5567fee85

--- index.js
+++ index.js
@@ -1,1 +1,50 @@
-// Your code here
+const array = [1,2,3,-9]
+
+function mapToNegativize(sourceArray){
+    return sourceArray.map(function(single){
+        return single * -1
+    })
+}
+
+function mapToNoChange(sourceArray){
+    return sourceArray
+}
+
+function mapToDouble(sourceArray){
+    return sourceArray.map(function(i){
+        return i * 2
+    })
+}
+
+function mapToSquare(sourceArray){
+    return sourceArray.map(function(i){
+        return i * i
+    })
+}
+
+function reduceToTotal(sourceArray, startingPoint = 0){
+    for (let index = 0; index < sourceArray.length; index++) {
+        startingPoint += sourceArray[index]
+    }
+    return startingPoint
+}
+
+function reduceToAllTrue(src) {
+    for (let i = 0; i < src.length; i++ ) {
+      if (!src[i]) return false
+    }
+    return true
+  }
+
+  function reduceToAnyTrue(sourceArray){
+    for (let i = 0; i < sourceArray.length; i++ ) {
+        if (sourceArray[i]) return true
+      }
+      return false
+  }
+
+
+
+
+
+
